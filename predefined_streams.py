c2d3d11121ccbbd7cc4fdc9b6936cd9f18ebaccb
tbnobed <tbnobed@gmail.com>
2025-09-19 05:49:04 +0000

    updated streams

--- predefined_streams.py
+++ predefined_streams.py
@@ -8,9 +8,22 @@ RTMP_STREAMS = {
     "Plex 6": "rtmp://cdn1.obedtv.live/live/livestream6",
     "Plex 7": "rtmp://cdn1.obedtv.live/live/livestream7",
     "Plex 8": "rtmp://cdn1.obedtv.live/live/livestream8",
-    "Socal 1": "rtmp://cdn1.obedtv.live/live/livestream8",
+    "Socal 1": "rtmp://cdn1.obedtv.live:1937/live/Socal1",
+    "Socal 2": "rtmp://cdn1.obedtv.live:1937/live/Socal2",
+    "Socal 3": "rtmp://cdn1.obedtv.live:1937/live/Socal3",
+    "Socal 4": "rtmp://cdn1.obedtv.live:1937/live/Socal4",
+    "Socal 5": "rtmp://cdn1.obedtv.live:1937/live/Socal5",
+    "Socal 6": "rtmp://cdn1.obedtv.live:1937/live/Socal6",
+    "Nashville 1": "rtmp://cdn1.obedtv.live:1937/live/Nashville1",
+    "Nashville 2": "rtmp://cdn1.obedtv.live:1937/live/Nashville2",
+    "Nashville 3": "rtmp://cdn1.obedtv.live:1937/live/Nashville3",
+    "Nashville 4": "rtmp://cdn1.obedtv.live:1937/live/Nashville4",
+    "Irving 1": "rtmp://cdn1.obedtv.live:1937/live/kdtx1",
+    "Irving 2": "rtmp://cdn1.obedtv.live:1937/live/kdtx2",   
+    "Irving 3": "rtmp://cdn1.obedtv.live:1937/live/kdtx3",
+    "Irving 4": "rtmp://cdn1.obedtv.live:1937/live/kdtx4",
+    "TBN Live": "https://6740bddc3aebe.streamlock.net/tbn-live/smil:monitor-tbn.smil/playlist.m3u8"
 
-    
 }
 
 # Dictionary containing M3U8 preview URLs
@@ -24,7 +37,20 @@ M3U8_STREAMS = {
     "Plex 6": "http://cdn1.obedtv.live:2022/live/livestream6.m3u8",
     "Plex 7": "http://cdn1.obedtv.live:2022/live/livestream7.m3u8",
     "Plex 8": "http://cdn1.obedtv.live:2022/live/livestream8.m3u8",
+    "Socal 1": "http://cdn1.obedtv.live:2024/live/Socal1.m3u8",
+    "Socal 2": "http://cdn1.obedtv.live:2024/live/Socal2.m3u8",
+    "Socal 3": "http://cdn1.obedtv.live:2024/live/Socal3.m3u8",
+    "Socal 4": "http://cdn1.obedtv.live:2024/live/Socal4.m3u8",
+    "Socal 5": "http://cdn1.obedtv.live:2024/live/Socal5.m3u8",
+    "Socal 6": "http://cdn1.obedtv.live:2024/live/Socal6.m3u8",
+    "Nashville 1": "http://cdn1.obedtv.live:2024/live/Nashville1.m3u8",
+    "Nashville 2": "http://cdn1.obedtv.live:2024/live/Nashville2.m3u8",
+    "Nashville 3": "http://cdn1.obedtv.live:2024/live/Nashville3.m3u8",
+    "Nashville 4": "http://cdn1.obedtv.live:2024/live/Nashville4.m3u8",
+    "Irving 1": "http://cdn1.obedtv.live:2024/live/kdtx1.m3u8",
+    "Irving 2": "http://cdn1.obedtv.live:2024/live/kdtx2.m3u8",
+    "Irving 3": "http://cdn1.obedtv.live:2024/live/kdtx3.m3u8",
+    "Irving 4": "http://cdn1.obedtv.live:2024/live/kdtx4.m3u8",
+    "TBN Live": "https://6740bddc3aebe.streamlock.net/tbn-live/smil:monitor-tbn.smil/playlist.m3u8"
 
-    
-   
 }
